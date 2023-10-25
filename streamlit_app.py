0a46da74a15a00b5b40c1c7636085ebfc48a47ba
SaifShaikh2608 <148771820+SaifShaikh2608@users.noreply.github.com>
2023-10-25 10:26:08 +0530

    Update streamlit_app.py
    my moms healthy dinner

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ created the main python file
 import stream lit 
 streamlit.title('My parents new healty diner')
 stramlit.header('Breakfast menu')
-streamlit.text('omega 3 & burbury oatmeal')
-streamlit.text('kale,spinach & rockect smoothie')
-stramlit.text('hard boiled free-range egg')
+streamlit.text('🥣omega 3 & burbury oatmeal')
+streamlit.text('🥗 kale,spinach & rockect smoothie')
+stramlit.text('🐔 hard boiled free-range egg')
+streamlit.text('🥑🍞 Avocado toast')
